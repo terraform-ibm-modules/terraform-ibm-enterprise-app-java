54f17e99b90778d7dbd5618b030446d53ead4b9d
valerio-bontempi <valerio.bontempi@it.ibm.com>
2024-10-11 18:26:54 +0200

    feat: added code to validate dashboard URL after deployment in the tests

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,7 +2,9 @@
 package test
 
 import (
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"testing"
 
@@ -57,17 +59,27 @@ func setupOptions(t *testing.T, prefix string, dir string, terraformVars map[str
 
 func TestRunBasicExample(t *testing.T) {
 	t.Parallel()
+	// TODO remove skip test
 	t.Skip("Skipping test until available in production IBM Cloud.")
 
 	options := setupOptions(t, "ease-basic", basicExampleDir, nil)
 
-	output, err := options.RunTestConsistency()
-	assert.Nil(t, err, "This should not have errored")
-	assert.NotNil(t, output, "Expected some output")
+	options.SkipTestTearDown = true
+	defer func() {
+		options.TestTearDown()
+	}()
+
+	_, err := options.RunTestConsistency()
+	if assert.Nil(t, err, "Consistency test should not have errored") {
+		// retrieving dashboard URL to perform HTTP GET request to confirm the application is successfully started
+		outputs := options.LastTestTerraformOutputs
+		assert.Equal(t, checkDashboardUrl(t, outputs), true, "Checking dashboardURL failed")
+	}
 }
 
 func TestRunCompleteExample(t *testing.T) {
 	t.Parallel()
+	// TODO remove skip test
 	t.Skip("Skipping test until available in production IBM Cloud.")
 
 	// not setting github token as the sample repositories are public
@@ -83,28 +95,17 @@ func TestRunCompleteExample(t *testing.T) {
 		options.TestTearDown()
 	}()
 
-	// // retrieving dashboard URL to perform curl to confirm the application is successfully deployed
-	// _, err := options.RunTestConsistency()
-	// if assert.Nil(t, err, "Consistency test should not have errored") {
-	// 	outputs := options.LastTestTerraformOutputs
-	// 	_, tfOutputsErr := testhelper.ValidateTerraformOutputs(outputs, "cluster_id")
-	// 	if assert.Nil(t, tfOutputsErr, tfOutputsErr) {
-
-	// 		// get cluster config
-	// 		cloudinfosvc, err := cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
-	// 		if assert.Nil(t, err, "Error creating cloud info service") {
-	// 			clusterConfigPath, err := cloudinfosvc.GetClusterConfigConfigPath(outputs["cluster_id"].(string))
-	// 		}
-	// 	}
-	// }
-
-	output, err := options.RunTestConsistency()
-	assert.Nil(t, err, "This should not have errored")
-	assert.NotNil(t, output, "Expected some output")
+	_, err := options.RunTestConsistency()
+	if assert.Nil(t, err, "Consistency test should not have errored") {
+		// retrieving dashboard URL to perform HTTP GET request to confirm the application is successfully started
+		outputs := options.LastTestTerraformOutputs
+		assert.Equal(t, checkDashboardUrl(t, outputs), true, "Checking dashboardURL failed")
+	}
 }
 
 func TestRunUpgradeExample(t *testing.T) {
 	t.Parallel()
+	// TODO remove skip test
 	t.Skip("Skipping test until available in production IBM Cloud.")
 
 	options := setupOptions(t, "ease-upgrade", basicExampleDir, nil)
@@ -114,4 +115,35 @@ func TestRunUpgradeExample(t *testing.T) {
 		assert.Nil(t, err, "This should not have errored")
 		assert.NotNil(t, output, "Expected some output")
 	}
+}
+
+// checking the dashboard URL in the terraform output
+// the response is expected to have (1) statusCode 200 (2) status 200 OK (3) lenght of the response greater than 0
+// returning true if the dashboard URL is found on the terraform output, if not empty, and if the checks on the response are successful, false otherwise
+func checkDashboardUrl(t *testing.T, terraformOutput map[string]interface{}) bool {
+	_, tfOutputsErr := testhelper.ValidateTerraformOutputs(terraformOutput, "ease_instance_dashboard_url")
+	if assert.Nil(t, tfOutputsErr, tfOutputsErr) {
+		dashboardUrl := terraformOutput["ease_instance_dashboard_url"].(string)
+		t.Logf("EASe instance dashboard Url going to be used is %s", dashboardUrl)
+		if assert.NotEmpty(t, dashboardUrl, "EASe application dashboardUrl retrieved from terraform output but it looks empty") {
+			resp, err := http.Get(dashboardUrl)
+			if assert.Nil(t, err, "Error in performing request towards dashboardURL") {
+				defer resp.Body.Close()
+				// collecting response details
+				statusCode := resp.StatusCode
+				status := resp.Status
+				respSize := resp.ContentLength
+				// reading body (body not used as not needed)
+				_, err := io.ReadAll(resp.Body)
+				if assert.Nil(t, err, "Error in reading response body") {
+					t.Logf("Got response from %s - statusCode %d - status %s - response size %d", dashboardUrl, statusCode, status, uint64(respSize))
+					if assert.Equal(t, 200, statusCode, "Response status code different from expected 200") && assert.Equal(t, "200 OK", status, "Response status different from expected '200 OK'") && assert.Greater(t, uint64(respSize), uint64(0), "Response size not greater than 0") {
+						t.Logf("All checks on response got from dashboard URL %s are successful", dashboardUrl)
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
 }

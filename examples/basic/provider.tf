140c2608884e404b19fe28f443bc457d1a624386
valerio-bontempi <valerio.bontempi@it.ibm.com>
2024-12-13 16:29:50 +0100

    fix: fixed subscriptionIDs in tests

--- examples/basic/provider.tf
+++ examples/basic/provider.tf
@@ -7,8 +7,9 @@ provider "ibm" {
   region           = var.region
 }
 
+# setting the region for the provider on the secrets manager region using the subscriptionID secret crn
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
-  region           = local.sm_region
+  region           = module.crn_parser_subid.region
   alias            = "ibm-sm"
 }
--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -45,24 +45,16 @@ locals {
 
 # parsing secret crn to collect the secrets manager ID, the region and the secret ID
 module "crn_parser_subid" {
-  count   = var.subscription_id_secret_crn != null ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
   version = "1.1.0"
   crn     = var.subscription_id_secret_crn
 }
 
-# setting the region for the provider on the secrets manager region
-# if null it is left to empty string
-locals {
-  sm_region = var.subscription_id_secret_crn != null ? module.crn_parser_subid[0].region : (var.repos_git_token_secret_crn != null ? module.crn_parser_token[0].region : "")
-}
-
 data "ibm_sm_arbitrary_secret" "sm_subscription_id" {
-  count       = var.subscription_id_secret_crn != null ? 1 : 0
-  instance_id = module.crn_parser_subid[0].service_instance
+  instance_id = module.crn_parser_subid.service_instance
   #checkov:skip=CKV_SECRET_6: does not require high entropy string as is static type
-  region    = module.crn_parser_subid[0].region
-  secret_id = module.crn_parser_subid[0].resource
+  region    = module.crn_parser_subid.region
+  secret_id = module.crn_parser_subid.resource
   provider  = ibm.ibm-sm
 }
 
@@ -80,7 +72,7 @@ module "ease" {
   config_repo       = var.config_repo
   source_repo       = var.source_repo
   repos_git_token   = local.repos_git_token
-  subscription_id   = data.ibm_sm_arbitrary_secret.sm_subscription_id[0].payload
+  subscription_id   = data.ibm_sm_arbitrary_secret.sm_subscription_id.payload
 }
 
 locals {
